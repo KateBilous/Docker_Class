fe8384c8f26a45429146c870704e7a14149a85a4
Kate Bilous <kbilous@slice.com>
2017-08-08 10:19:39 +0300

    added Dicker file

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo 'Initialization database...'
+
+service mysql start
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE discovery"
+
+mysql -uroot -p$MYSQL_ROOT_PASSWORD discovery < createdb.sql
+
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT SELECT ON discovery.* TO 'root'@'%' IDENTIFIED BY 'root'; FLUSH PRIVILEGES"
+
+echo 'Database initialized'
